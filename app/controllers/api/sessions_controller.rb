f62b0ad3d0cb8e30eac4c6a722dbd8b42418d7b3
Aaron Young <brainomite@gmail.com>
2018-09-11 14:47:54 -0400

    fix: Change sessions_controller to use email

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::SessionsController < ApplicationController
   def create
     @user = User.find_by_credentials(
-      params[:user][:name],
+      params[:user][:email],
       params[:user][:password]
     )
     if @user
