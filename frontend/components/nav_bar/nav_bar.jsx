11251ee03005cd70c80152be145d26c30776075e
Aaron Young <brainomite@gmail.com>
2018-09-12 15:23:46 -0400

    Add styling to navbar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -5,22 +5,24 @@ const NavBar = (props) => {
   const loggedIn = Boolean(props.currentUser);
   return (
     <header className="navbar">
-      <h1><Link to="/" className="logo">weMeet</Link></h1>
-      <nav>
-        <ul className="navbar-links">
-          <li className="navbar-links-newgroup">Start a new group</li>
-          {loggedIn ? null : <li><Link to="/login">Log In</Link></li>}
-          {loggedIn ? null : <li><Link to="/signup">Sign Up</Link></li>}
-          {loggedIn
-            ? <li>{props.currentUser.name}</li>
-            : null
-          }
-          {loggedIn
-            ? <li><button onClick={props.logout}>logout</button></li>
-            : null
-          }
-        </ul>
-      </nav>
+      <div className="navbar-main">
+        <Link to="/" className="logo">weMeet</Link>
+        <nav>
+          <ul className="navbar-main-links">
+            <li className="navbar-main-links-newgroup">Start a new group</li>
+            {loggedIn ? null : <li><Link to="/login">Log In</Link></li>}
+            {loggedIn ? null : <li><Link to="/signup">Sign Up</Link></li>}
+            {loggedIn
+              ? <li>{props.currentUser.name}</li>
+              : null
+            }
+            {loggedIn
+              ? <li><button onClick={props.logout}>logout</button></li>
+              : null
+            }
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
