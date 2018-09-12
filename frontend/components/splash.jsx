07d65db536970a4b3fc62a7364d1b03eb2653c42
Aaron Young <brainomite@gmail.com>
2018-09-12 17:34:56 -0400

    Make splash signup button functional

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'
 const VIDEO_URL = "https://www.meetup.com/mu_static/en-US/dddafbfe4574fc19c6718950691dcb78.mp4"; // eslint-disable-line max-len
 const Splash = (props) => {
   return (
@@ -16,10 +17,15 @@ const Splash = (props) => {
       <section className="splash-section">
         <h1>What do you love?</h1>
         <h3>Do more of it with WeMeet</h3>
-        <div className="splash-section-button">Sign Up</div>
+        <div
+          className="splash-section-button"
+          onClick={()=>props.history.push('/signup')}
+        >
+          <span>Sign Up</span>
+        </div>
       </section>
     </main>
   );
 };
 
-export default Splash;
+export default withRouter(Splash);
