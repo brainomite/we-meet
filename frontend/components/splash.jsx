128c53927cbdf51a959e7a6ed4c42348541e3c6c
Aaron Young <brainomite@gmail.com>
2018-09-12 17:08:57 -0400

    Add button stylings for splash

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-
+const VIDEO_URL = "https://www.meetup.com/mu_static/en-US/dddafbfe4574fc19c6718950691dcb78.mp4"; // eslint-disable-line max-len
 const Splash = (props) => {
   return (
-    <main>
+    <main className="splash">
       <video
         autoPlay
         loop
         className="splash-video"
         >
         <source
-          src="https://www.meetup.com/mu_static/en-US/dddafbfe4574fc19c6718950691dcb78.mp4"
+          src={VIDEO_URL}
           type="video/mp4"
-          />Your browser does not support the video tag.</
-          video>
-          <h1>stuff!</h1>
+        />Your browser does not support the video tag.</
+      video>
+      <section className="splash-section">
+        <h1>What do you love?</h1>
+        <h3>Do more of it with WeMeet</h3>
+        <div className="splash-section-button">Sign Up</div>
+      </section>
     </main>
   );
 };
