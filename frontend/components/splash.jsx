c83246103664b0daa3e5213736cd1586ce00a82e
Aaron Young <brainomite@gmail.com>
2018-09-13 16:03:29 -0400

    Fix shading of the splash

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom'
-const VIDEO_URL = "https://www.meetup.com/mu_static/en-US/dddafbfe4574fc19c6718950691dcb78.mp4"; // eslint-disable-line max-len
+import { withRouter } from 'react-router-dom';
+// eslint-disable-next-line max-len
+const VIDEO_URL = "https://www.meetup.com/mu_static/en-US/dddafbfe4574fc19c6718950691dcb78.mp4";
+
 const Splash = (props) => {
   return (
     <main className="splash">
-      <video
-        autoPlay
-        loop
-        className="splash-video"
-        >
-        <source
-          src={VIDEO_URL}
-          type="video/mp4"
-        />Your browser does not support the video tag.</
-      video>
-      <section className="splash-section">
-        <h1>What do you love?</h1>
-        <h3>Do more of it with WeMeet</h3>
-        <div
-          className="splash-section-button"
-          onClick={()=>props.history.push('/signup')}
-        >
-          <span>Sign Up</span>
-        </div>
-      </section>
+        <video
+          autoPlay
+          loop
+          className="splash-video"
+          >
+          <source
+            src={VIDEO_URL}
+            type="video/mp4"
+            />Your browser does not support the video tag.</
+            video>
+            <div className="splash-black-div">
+            </div>
+            <section className="splash-section">
+              <h1>What do you love?</h1>
+              <h3>Do more of it with WeMeet</h3>
+              <div
+                className="splash-section-button"
+                onClick={()=>props.history.push('/signup')}
+                >
+                <span>Sign Up</span>
+              </div>
+            </section>
     </main>
   );
 };
