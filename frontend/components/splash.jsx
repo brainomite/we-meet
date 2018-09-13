b1b150a9fa32a0b4ecbc786996299d928dd375dd
Aaron Young <brainomite@gmail.com>
2018-09-13 16:04:14 -0400

    Fix styling

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -6,28 +6,28 @@ const VIDEO_URL = "https://www.meetup.com/mu_static/en-US/dddafbfe4574fc19c67189
 const Splash = (props) => {
   return (
     <main className="splash">
-        <video
-          autoPlay
-          loop
-          className="splash-video"
-          >
-          <source
-            src={VIDEO_URL}
-            type="video/mp4"
-            />Your browser does not support the video tag.</
-            video>
-            <div className="splash-black-div">
+      <video
+        autoPlay
+        loop
+        className="splash-video"
+        >
+        <source
+          src={VIDEO_URL}
+          type="video/mp4"
+          />Your browser does not support the video tag.</
+          video>
+          <div className="splash-black-div">
+          </div>
+          <section className="splash-section">
+            <h1>What do you love?</h1>
+            <h3>Do more of it with WeMeet</h3>
+            <div
+              className="splash-section-button"
+              onClick={()=>props.history.push('/signup')}
+              >
+              <span>Sign Up</span>
             </div>
-            <section className="splash-section">
-              <h1>What do you love?</h1>
-              <h3>Do more of it with WeMeet</h3>
-              <div
-                className="splash-section-button"
-                onClick={()=>props.history.push('/signup')}
-                >
-                <span>Sign Up</span>
-              </div>
-            </section>
+          </section>
     </main>
   );
 };
