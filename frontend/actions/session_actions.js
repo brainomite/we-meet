46e1df12e9f10b123b62303c4b0b6e5f1ee1405d
Aaron Young <brainomite@gmail.com>
2018-09-14 10:51:15 -0400

    Add user error reducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,66 +1,73 @@
-import * as SessionAPIUtil from './../util/session_api_util';
+import * as SessionAPIUtil from "./../util/session_api_util";
 
-export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
-export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
-export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+export const RECEIVE_USER_ERRORS = "RECEIVE_USER_ERRORS";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 
-const receiveErrors = (errors) => {
+const receiveSessionErrors = errors => {
   return {
     type: RECEIVE_SESSION_ERRORS,
     errors: errors.responseJSON
   };
 };
 
-const logoutCurrentUser = () => {
+const receiveUserErrors = errors => {
   return {
-    type: LOGOUT_CURRENT_USER,
+    type: RECEIVE_USER_ERRORS,
+    errors: errors.responseJSON
   };
 };
 
-const receiveCurrentUser = (currentUser) => {
+const logoutCurrentUser = () => {
+  return {
+    type: LOGOUT_CURRENT_USER
+  };
+};
+
+const receiveCurrentUser = currentUser => {
   return {
     type: RECEIVE_CURRENT_USER,
     user: currentUser
   };
 };
 
-export const login = (user) => {
-  return (dispatch) => {
-    const success = (result) => {
+export const login = user => {
+  return dispatch => {
+    const success = result => {
       return dispatch(receiveCurrentUser(result));
     };
 
-    const failure = (errorResults) => {
-      return dispatch(receiveErrors(errorResults));
+    const failure = errorResults => {
+      return dispatch(receiveSessionErrors(errorResults));
     };
 
     SessionAPIUtil.login(user).then(success, failure);
   };
 };
 
-export const signup = (user) => {
-  return (dispatch) => {
-    const success = (result) => {
+export const signup = user => {
+  return dispatch => {
+    const success = result => {
       return dispatch(receiveCurrentUser(result));
     };
 
-    const failure = (errorResults) => {
-      return dispatch(receiveErrors(errorResults));
+    const failure = errorResults => {
+      return dispatch(receiveUserErrors(errorResults));
     };
 
     SessionAPIUtil.signup(user).then(success, failure);
   };
 };
 
-
 export const logout = () => {
-  return (dispatch) => {
+  return dispatch => {
     const success = () => {
       return dispatch(logoutCurrentUser());
     };
 
-    const failure = (errorResults) => {
-      return dispatch(receiveErrors(errorResults));
+    const failure = errorResults => {
+      return dispatch(receiveSessionErrors(errorResults));
     };
 
     SessionAPIUtil.logout().then(success, failure);
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,9 +1,11 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-import sessionErrorsReducer from './session_errors_reducer';
+import sessionErrorsReducer from "./session_errors_reducer";
+import userErrorsReducer from "./user_errors_reducer";
 
 const errorsReducer = combineReducers({
-  session: sessionErrorsReducer
+  session: sessionErrorsReducer,
+  user: userErrorsReducer
 });
 
 export default errorsReducer;
