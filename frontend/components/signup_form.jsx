d77f7807f053d4164613030253956347cb59f594
Aaron Young <brainomite@gmail.com>
2018-09-13 22:30:25 -0400

    Start styling signup_form

--- frontend/components/signup_form.jsx
+++ frontend/components/signup_form.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { signup } from '../actions/session_actions.js';
+import React from "react";
+import { connect } from "react-redux";
+import { signup } from "../actions/session_actions.js";
 
 class SignupForm extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      name: '', email: '', password: '',
+      name: "",
+      email: "",
+      password: "",
       hometown_id: 1 // hard coded temporarily
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event){
+  handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value
     });
   }
 
-  handleSubmit(event){
+  handleSubmit(event) {
     event.preventDefault();
     const userObj = {
       user: this.state
@@ -27,38 +29,49 @@ class SignupForm extends React.Component {
     this.props.signup(userObj);
   }
 
-  render () {
+  render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>Name:
-          <input type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>Email:
-          <input type="text"
-            name="email"
-            onChange={this.handleChange}
-            value={this.state.email}
-          />
-        </label>
-        <label>password:
-          <input type="text"
-            onChange={this.handleChange}
-            name="password"
-            value={this.state.password}
-          />
-        </label>
-        <input type="submit" />
-      </form>
+      <main className="signup">
+        <div className="signup-form">
+          <form onSubmit={this.handleSubmit}>
+            <h1 className="signup-form-header">Sign Up</h1>
+            <p>Name:</p>
+            <input
+              type="text"
+              name="name"
+              className="signup-form-input"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+            <p>Email:</p>
+            <input
+              type="text"
+              name="email"
+              className="signup-form-input"
+              onChange={this.handleChange}
+              value={this.state.email}
+            />
+            <p>password:</p>
+            <input
+              type="text"
+              className="signup-form-input"
+              onChange={this.handleChange}
+              name="password"
+              value={this.state.password}
+            />
+            <input type="submit" />
+          </form>
+        </div>
+      </main>
     );
   }
 }
 
-const mdp = (dispatch) => ({
-  signup: (user)=>dispatch(signup(user))
+const mdp = dispatch => ({
+  signup: user => dispatch(signup(user))
 });
 
-export default connect(null, mdp)(SignupForm);
+export default connect(
+  null,
+  mdp
+)(SignupForm);
