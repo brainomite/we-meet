1bb71dd1ff075d47132ca304b8422657b5a8244f
Aaron Young <brainomite@gmail.com>
2018-09-16 11:10:46 -0400

    Add classes to avatar_form for styling

--- frontend/components/avatar_form.jsx
+++ frontend/components/avatar_form.jsx
@@ -25,16 +25,26 @@ const AvatarForm = props => {
     <span className="far fa-user-circle" />
   );
   return (
-    <main className="welcome">
-      <h1>Welcome!</h1>
-      <p>Add a photo so other members know who you are.</p>
-      {preview}
-      <label>
-        Upload a photo
-        <input type="file" onChange={handleFile(props)} />
-      </label>
-      <p>Or</p>
-      <Link to="/">Skip for now</Link>
+    <main className="avatarForm">
+      <div className="avatarForm-body">
+        <h1 className="avatarForm-body-header">Welcome!</h1>
+        <p className="avatarForm-body-instruction">
+          Add a photo so other members know who you are.
+        </p>
+        <div className="avatarForm-body-avatar">{preview}</div>
+        <label className="avatarForm-body-button">
+          Upload a photo
+          <input
+            className="avatarForm-body-input"
+            type="file"
+            onChange={handleFile(props)}
+          />
+        </label>
+        <p className="avatarForm-body-or">Or</p>
+        <Link className="avatarForm-body-skipButton" to="/">
+          Skip for now
+        </Link>
+      </div>
     </main>
   );
 };
