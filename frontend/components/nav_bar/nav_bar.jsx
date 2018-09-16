30ad6ed3c94355fed8d04f81ab051820e826a91d
Aaron Young <brainomite@gmail.com>
2018-09-16 17:12:09 -0400

    Make navbar hide links at certain routes

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const NavBar = (props) => {
+const PATHS_TO_HIDE_LINKS = ["/welcome", "/signup"];
+
+const NavBar = props => {
   const loggedIn = Boolean(props.currentUser);
   return (
     <header className="navbar">
       <div className="navbar-main">
-        <Link to="/" className="logo">WeMeet</Link>
+        <Link to="/" className="logo">
+          WeMeet
+        </Link>
         <nav>
-          <ul className="navbar-main-links">
-            <li className="navbar-main-links-newgroup">Start a new group</li>
-            {loggedIn ? null : <li><Link to="/login">Log In</Link></li>}
-            {loggedIn ? null : <li><Link to="/signup">Sign Up</Link></li>}
-            {loggedIn
-              ? <li>{props.currentUser.name}</li>
-              : null
-            }
-            {loggedIn
-              ? <li><button onClick={props.logout}>logout</button></li>
-              : null
-            }
-          </ul>
+          {PATHS_TO_HIDE_LINKS.includes(props.location.pathname) ? null : (
+            <ul className="navbar-main-links">
+              <li className="navbar-main-links-newgroup">Start a new group</li>
+              {loggedIn ? null : (
+                <li>
+                  <Link to="/login">Log In</Link>
+                </li>
+              )}
+              {loggedIn ? null : (
+                <li>
+                  <Link to="/signup">Sign Up</Link>
+                </li>
+              )}
+              {loggedIn ? <li>{props.currentUser.name}</li> : null}
+              {loggedIn ? (
+                <li>
+                  <button onClick={props.logout}>logout</button>
+                </li>
+              ) : null}
+            </ul>
+          )}
         </nav>
       </div>
     </header>
--- frontend/components/nav_bar/nav_bar_container.jsx
+++ frontend/components/nav_bar/nav_bar_container.jsx
@@ -1,13 +1,19 @@
-import { connect } from 'react-redux';
-import { logout } from '../../actions/session_actions.js';
-import NavBar from './nav_bar.jsx';
+import { connect } from "react-redux";
+import { logout } from "../../actions/session_actions.js";
+import { withRouter } from "react-router-dom";
+import NavBar from "./nav_bar.jsx";
 
-const msp = ({entities, session}) => ({
+const msp = ({ entities, session }) => ({
   currentUser: entities.users[session.id]
 });
 
-const mdp = (dispatch) => ({
-  logout: ()=>dispatch(logout())
+const mdp = dispatch => ({
+  logout: () => dispatch(logout())
 });
 
-export default connect(msp, mdp)(NavBar);
+export default withRouter(
+  connect(
+    msp,
+    mdp
+  )(NavBar)
+);
