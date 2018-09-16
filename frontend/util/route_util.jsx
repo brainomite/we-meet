57f786d5cf4ce6155592b6b2d8669788ad18533c
Aaron Young <brainomite@gmail.com>
2018-09-15 21:02:58 -0400

    Upadte route_util

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -1,20 +1,44 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { withRouter, Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Redirect } from "react-router-dom";
 
-const AuthRoute = ({component: Component, path, loggedIn, exact}) => {
-  return (
-  <Route path={path} exact={exact} render={(props) => (
-    !loggedIn ? (
-      <Component {...props} />
-    ) : (
-      <Redirect to="/" />
-    )
-  )}/>
-);};
-
-const mapStateToProps = state => {
-  return {loggedIn: Boolean(state.session.id)};
+const AuthRoute = ({
+  component: Component,
+  path,
+  loggedIn,
+  exact,
+  requireLogin
+}) => {
+  if (requireLogin) {
+    return (
+      <Route
+        path={path}
+        exact={exact}
+        render={props =>
+          loggedIn ? <Component {...props} /> : <Redirect to="/register" />
+        }
+      />
+    );
+  } else {
+    return (
+      <Route
+        path={path}
+        exact={exact}
+        render={props =>
+          !loggedIn ? <Component {...props} /> : <Redirect to="/" />
+        }
+      />
+    );
+  }
 };
 
-export default withRouter(connect(mapStateToProps, null)(AuthRoute));
+const mapStateToProps = state => {
+  return { loggedIn: Boolean(state.session.id) };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    null
+  )(AuthRoute)
+);
