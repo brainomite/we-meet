ca86cac689c1d3ea6e45a4ebd6dd62658a89fea1
Aaron Young <brainomite@gmail.com>
2018-09-16 15:58:34 -0400

    Create framework for modal loading

---
+++ frontend/components/modal.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Loading from '../loading';
+
+function Modal({ modal }) {
+  if (!modal) {
+    return null;
+  }
+  let component;
+  switch (modal) {
+    case 'loading':
+    component = <Loading />;
+      break;
+    default:
+      return null;
+  }
+  return (
+    <div className="modal-background" >
+      <div className="modal-child" onClick={e => e.stopPropagation()}>
+        { component }
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    modal: state.ui.modal
+  };
+};
+
+
+export default connect(mapStateToProps)(Modal);
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,10 +1,12 @@
-import { combineReducers } from 'redux';
-import sessionReducer from './session_reducer';
-import entitiesReducer from './entities_reducer';
-import errorsReducer from './errors_reducer';
+import { combineReducers } from "redux";
+import sessionReducer from "./session_reducer";
+import entitiesReducer from "./entities_reducer";
+import errorsReducer from "./errors_reducer";
+import uiReducer from "./ui_reducer";
 
 export default combineReducers({
   session: sessionReducer,
   entities: entitiesReducer,
   errors: errorsReducer,
+  ui: uiReducer
 });
