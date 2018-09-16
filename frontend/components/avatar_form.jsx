1f38866bcd64778de9428a4f39d83389c8ecd815
Aaron Young <brainomite@gmail.com>
2018-09-16 11:03:01 -0400

    Convert avatar_form from a class to functional component

--- frontend/components/avatar_form.jsx
+++ frontend/components/avatar_form.jsx
@@ -3,50 +3,43 @@ import { Link } from "react-router-dom";
 import { setAvatar } from "./../actions/session_actions";
 import { connect } from "react-redux";
 
-class AvatarForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      photoUrl: null
-    };
-  }
-
-  handleFile(e) {
-    const file = e.currentTarget.files[0];
+const handleFile = props => {
+  return event => {
+    const file = event.currentTarget.files[0];
     const fileReader = new FileReader();
     fileReader.onloadend = () => {
       const formData = new FormData();
       formData.append("avatar", file);
-      this.props.setAvatar(formData);
-      // this.setState({ photoFile: file, photoUrl: fileReader.result });
+      props.setAvatar(formData);
     };
     if (file) {
       fileReader.readAsDataURL(file);
     }
-  }
+  };
+};
 
-  render() {
-    const preview = this.props.currentUser.avatarUrl ? (
-      <img src={this.props.currentUser.avatarUrl} />
-    ) : (
-      <span className="far fa-user-circle" />
-    );
-    return (
-      <main className="welcome">
-        <h1>Welcome!</h1>
-        <p>Add a photo so other members know who you are.</p>
-        {preview}
-        <label>
-          Upload a photo
-          <input type="file" onChange={this.handleFile.bind(this)} />
-        </label>
-        <p>Or</p>
-        <Link to="/">Skip for now</Link>
-      </main>
-    );
-  }
-}
-const msp = ({entities, session}) => ({
+const AvatarForm = props => {
+  const preview = props.currentUser.avatarUrl ? (
+    <img src={props.currentUser.avatarUrl} />
+  ) : (
+    <span className="far fa-user-circle" />
+  );
+  return (
+    <main className="welcome">
+      <h1>Welcome!</h1>
+      <p>Add a photo so other members know who you are.</p>
+      {preview}
+      <label>
+        Upload a photo
+        <input type="file" onChange={handleFile(props)} />
+      </label>
+      <p>Or</p>
+      <Link to="/">Skip for now</Link>
+    </main>
+  );
+};
+
+const msp = ({ entities, session }) => ({
   currentUser: entities.users[session.id]
 });
 
