350598f89eec3c642ab4e7bc8ffc5dc0e1a38046
Aaron Young <brainomite@gmail.com>
2018-09-20 09:07:04 -0400

    Cleanup feat/auth branch of dev tests

--- frontend/we_meet.jsx
+++ frontend/we_meet.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import configureStore from './store/store';
-import Root from './components/root';
+import React from "react";
+import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   let store;
   if (window.currentUser) {
     const preloadedState = {
@@ -17,19 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-  const rootEl = document.getElementById('root');
+  const rootEl = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, rootEl);
-
-  // *** for testing only - to be removed! ***
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
 });
-
-
-// for testing only - to be removed!
-import * as sessionApiUtil from './util/session_api_util';
-import { login, signup, logout } from './actions/session_actions';
-window.sessionApiUtil = sessionApiUtil;
-window.login = login;
-window.signup = signup;
-window.logout = logout;
