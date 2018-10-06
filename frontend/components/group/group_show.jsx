39be9e5d99c22fe3a2c9274d8833ba3c2542181a
Aaron Young <brainomite@gmail.com>
2018-10-06 16:31:47 -0400

    feat: Begin styling of group_show header

--- frontend/components/group/group_show.jsx
+++ frontend/components/group/group_show.jsx
@@ -16,13 +16,31 @@ class GroupShow extends React.Component {
   }
 
   render() {
+    const group = this.props.group;
+    // todo: fix mock
+    // const memberCount =  group.member_ids.length;
+    const memberCount = 2;
+    const memberOrMember = memberCount === 1 ? "member" : "members";
     return (
-      <main>
-        Welcome to group! {this.props.match.params.groupId}
+      <main className="group">
+        <header className="group-header">
+          <div className="group-header-container">
+            <div className="group-header-image" />
+            <div>
+              <h1>{group.name}</h1>
+              <ul>
+                <li>{group.hometown}</li>
+                <li>
+                  {memberCount} {memberOrMember}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </header>
         <section>
-          <div>name: {this.props.group.name}</div>
-          <div>description: {this.props.group.description}</div>
-          <div>hometown: {this.props.group.hometown}</div>
+          <div>name: {group.name}</div>
+          <div>description: {group.description}</div>
+          <div>hometown: {group.hometown}</div>
           <button className="confirm-button">Join</button>
           <br />
           <button className="confirm-button">Leave</button>
