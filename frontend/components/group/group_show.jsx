339b8f9738627214b19a90d3133e6bc5d4d324b2
Aaron Young <brainomite@gmail.com>
2018-10-07 10:40:37 -0400

    refactor: Refactor meet_show to clean up code.

--- frontend/components/group/group_show.jsx
+++ frontend/components/group/group_show.jsx
@@ -3,59 +3,24 @@ import React from "react";
 class GroupShow extends React.Component {
   constructor(props) {
     super(props);
+    this.groupHeader = this.groupHeader.bind(this);
+    this.groupNav = this.groupNav.bind(this);
   }
-
   componentDidMount() {
     this.props.fetchGroup(this.props.match.params.groupId);
   }
-
   componentWillReceiveProps(nextProps) {
     if (this.props.match.params.groupId !== nextProps.match.params.groupId) {
       this.props.fetchGroup(nextProps.match.params.groupId);
     }
   }
-  // src={window.wemeetAssets["icon_padlock.gif"]
   render() {
+    const { groupHeader: GroupHeader, groupNav: GroupNav } = this;
     const group = this.props.group;
-    const imgStyleObj = {
-      backgroundImage: `url(${window.wemeetAssets.defaultGroupImage})`,
-    };
-    // todo: fix mock
-    // const memberCount =  group.member_ids.length;
-    const memberCount = 2;
-    const memberOrMember = memberCount === 1 ? "member" : "members";
     return (
       <main className="group">
-        <header className="group-header">
-          <div className="group-header-container">
-            <div>
-              <div>
-                <div className="group-header-image" style={imgStyleObj} />
-              </div>
-              <section>
-                <h1>{group.name}</h1>
-                <ul>
-                  <li>{group.hometown}</li>
-                  <li>
-                    {memberCount} {memberOrMember}
-                  </li>
-                </ul>
-              </section>
-            </div>
-          </div>
-        </header>
-        <nav id="group-nav">
-          <div>
-            <ul>
-              <li>About</li>
-              <li>members</li>
-            </ul>
-            <section>
-              <button className="confirm-button">Join</button>
-              <button className="confirm-button">Leave</button>
-            </section>
-          </div>
-        </nav>
+        <GroupHeader />
+        <GroupNav />
         <section>
           <div>name: {group.name}</div>
           <div>description: {group.description}</div>
@@ -64,6 +29,51 @@ class GroupShow extends React.Component {
       </main>
     );
   }
+  groupHeader() {
+    const group = this.props.group;
+    // todo: fix mock
+    // const memberCount =  group.member_ids.length;
+    const memberCount = 2;
+    const memberOrMember = memberCount === 1 ? "member" : "members";
+    const imgStyleObj = {
+      backgroundImage: `url(${window.wemeetAssets.defaultGroupImage})`,
+    };
+    return (
+      <header className="group-header">
+        <div className="group-header-container">
+          <div>
+            <div>
+              <div className="group-header-image" style={imgStyleObj} />
+            </div>
+            <section>
+              <h1>{group.name}</h1>
+              <ul>
+                <li>{group.hometown}</li>
+                <li>
+                  {memberCount} {memberOrMember}
+                </li>
+              </ul>
+            </section>
+          </div>
+        </div>
+      </header>
+    );
+  }
+  groupNav() {
+    return (
+      <nav id="group-nav">
+        <div>
+          <ul>
+            <li>About</li>
+            <li>members</li>
+          </ul>
+          <section>
+            <button className="confirm-button">Join</button>
+            <button className="confirm-button">Leave</button>
+          </section>
+        </div>
+      </nav>
+    );
+  }
 }
-
 export default GroupShow;
