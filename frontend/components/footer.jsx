c8bf640f5a923468105f88e03c77617c16a3e681
Aaron Young <brainomite@gmail.com>
2018-10-18 14:34:58 -0400

    feat: Add links to footer for GitHub and LinkedIn

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const Footer = (props) => {
+const Footer = props => {
   return (
     <footer className="footer">
       <span>Created with love by Aaron Young</span>
+      <div>
+        <a
+          href="https://www.linkedin.com/in/aaron-young2018/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fab fa-linkedin" />
+        </a>
+        <a
+          href="https://github.com/brainomite/we-meet"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fab fa-github" />
+        </a>
+      </div>
     </footer>
   );
 };
