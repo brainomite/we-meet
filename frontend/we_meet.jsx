a3642414923205a254e7e5d4dc8d4f63b5263e9c
Aaron Young <brainomite@gmail.com>
2018-10-25 17:28:06 -0400

    feat: Remove test items from window

--- frontend/we_meet.jsx
+++ frontend/we_meet.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
-const isDev = process.env.NODE_ENV !== "production";
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
@@ -21,28 +20,4 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   const rootEl = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, rootEl);
-  // for testing only - to be removed!
-  if (isDev){
-    window.dispatch = store.dispatch;
-  }
 });
-
-// for testing only - to be removed!
-import * as groupApiUtil from "./util/group_api_util";
-import {
-  fetchGroup,
-  fetchGroups,
-  createGroup,
-  joinGroup,
-  leaveGroup,
-  updateGroup,
-} from "./actions/group_actions";
-if (isDev) {
-  window.groupApiUtil = groupApiUtil;
-  window.fetchGroup = fetchGroup;
-  window.fetchGroups = fetchGroups;
-  window.createGroup = createGroup;
-  window.updateGroup = updateGroup;
-  window.joinGroup = joinGroup;
-  window.leaveGroup = leaveGroup;
-}
